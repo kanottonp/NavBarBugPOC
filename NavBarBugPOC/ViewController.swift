50f584221b79866553aed756cc28cec02f6a805b
Athipat Nampetch <athipat@ltman.com>
2020-07-22 14:47:23 +0700

    Initial Commit

---
+++ NavBarBugPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavBarBugPOC
+//
+//  Created by Athipat Nampetch on 22/7/2563 BE.
+//  Copyright © 2563 Athipat Nampetch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
